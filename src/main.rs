976d0254e537e1dab57a0d61ed33f9f6534275e5
renan-eng <renan3838@gmail.com>
2023-01-20 12:59:19 -0300

    rust intrudução variáveis

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() 
+{
+    let mut x = 45;
+    // Podrão em rust todas as variáveis são imutáveis
+    // mut transforma uma várias imutáveis em um variável mutável
+
+    println!("O valor de x é {}", x);
+
+    x = 60;
+
+    println!("O valor de x é {}", x);
+
+}
