a52d0276561617d807f896e53f6a7e5fcaa136a0
renan-eng <renan3838@gmail.com>
2023-01-20 13:07:40 -0300

    Adicionando: alguns exemplos de tipos de variáveis

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 fn main() 
 {
-    let mut x = 45;
     // Podrão em rust todas as variáveis são imutáveis
     // mut transforma uma várias imutáveis em um variável mutável
+    let mut x = 45; // 45 é to tipo i32 por padrão integer 32 bit
+    let y: i64 = 45; // i64 - integer 64bit
+    let _inteiro_positivo: u64 = 54; // u64 - Unsigned 64 bit
+    let _float: f32 = 6.7; // f32 floating 
+    let _verdade: bool = false; //boolean
+
 
     println!("O valor de x é {}", x);
+    println!("O valor de y é {}", y);
 
     x = 60;
 
